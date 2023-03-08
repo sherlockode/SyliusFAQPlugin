63e138198ede1b8be8cc3a1cef6845568625480a
simbr <brown.simon.a@gmail.com>
2023-03-08 12:09:00 +0100

    Remove position from CRUD

--- src/Form/Type/CategoryType.php
+++ src/Form/Type/CategoryType.php
@@ -9,11 +9,9 @@ use Sylius\Component\Core\Model\Channel;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Count;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CategoryType extends AbstractType
 {
@@ -23,12 +21,6 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('position', NumberType::class, [
-                'label' => 'sherlockode_sylius_faq.ui.category.position',
-                'constraints' => [
-                    new NotBlank(groups: ['sylius']),
-                ],
-            ])
             ->add('translations', ResourceTranslationsType::class, [
                 'entry_type' => CategoryTranslationType::class,
                 'label' => false,
